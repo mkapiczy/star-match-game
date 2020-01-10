4711ec55c43aadcd00b3ffc92bb63eaf04c45c2f
Michal Kapiczynski <michal.kapiczynski@grapeup.com>
2020-01-10 17:21:53 +0100

    Add game timer

--- src/components/Game/Game.tsx
+++ src/components/Game/Game.tsx
@@ -12,8 +12,9 @@ import CornerCircularLabel from '../CornerCircularLabel/CornerCurcularLabel';
 import {Corner} from "../../CommonTypes";
 import Trophy from "../Trophy/Trophy";
 import Bomb from "../Bomb/Bomb";
+import Timer from "../Timer/Timer"
+import {randomSum} from "../../services/math-utils";
 
-import {random, randomSum} from "../../services/math-utils";
 
 enum GameState {
     WON,
@@ -22,32 +23,48 @@ enum GameState {
 }
 
 const Game: React.FC = () => {
-    const maxNumberOfStars = 9;
-    const maxNumberOfRetries = 5;
+    const MAX_NUMBER_OF_STARS = 9;
+    const MAX_NUMBER_OF_RETRIES = 5;
+    const GAME_TIME = 10
+    const TIMER_GAIN = 2
     const [roundNumber, setRoundNumber] = useState(1);
     const [availableNumbers, setAvailableNumbers] = useState(Array.from(Array(9), (_, i) => i + 1))
-    const [numberOfStars, setNumberOfStars] = useState<number>(randomSum(availableNumbers, maxNumberOfStars))
+    const [numberOfStars, setNumberOfStars] = useState<number>(randomSum(availableNumbers, MAX_NUMBER_OF_STARS))
     const [selectedNumbers, setSelectedNumbers] = useState<Array<number>>([])
     const [numberOfRetries, setNumberOfRetries] = useState<number>(0);
     const [gameState, setGameState] = useState<GameState>(GameState.IN_PROGRESS)
-
+    const [timer, setTimer] = useState<number>(GAME_TIME)
     useEffect(() => {
         if (gameState === GameState.IN_PROGRESS) {
             computeGameState()
+            const timerId = setTimeout(() => {
+                decrementTimer()
+            }, 1000)
+            return () => clearTimeout(timerId);
         }
     })
+
+
+    const decrementTimer = () => {
+        if (timer >= 1) {
+            setTimer(timer - 1)
+        } else {
+            setGameState(GameState.LOST)
+        }
+    }
 
     const approveSelectedNumbers = () => {
         if (areSelectedNumbersCorrect()) {
             setSelectedNumbers([])
-            setNumberOfStars(randomSum(availableNumbers, maxNumberOfStars))
+            setNumberOfStars(randomSum(availableNumbers, MAX_NUMBER_OF_STARS))
             setRoundNumber(roundNumber + 1)
+            setTimer(timer + TIMER_GAIN)
         }
     }
 
     const retryDraw = () => {
         if (areRetriesEnabled()) {
-            setNumberOfStars(randomSum(availableNumbers, maxNumberOfStars))
+            setNumberOfStars(randomSum(availableNumbers, MAX_NUMBER_OF_STARS))
             setNumberOfRetries(numberOfRetries + 1)
         }
     }
@@ -55,12 +72,12 @@ const Game: React.FC = () => {
     const computeGameState = () => {
         if (!availableNumbers.length && !selectedNumbers.length) {
             setGameState(GameState.WON)
-        } else if (numberOfRetries === maxNumberOfRetries && availableNumbers.reduce((a, b) => a + b, 0) !== numberOfStars) {
+        } else if (numberOfRetries === MAX_NUMBER_OF_RETRIES && availableNumbers.reduce((a, b) => a + b, 0) !== numberOfStars) {
             setGameState(GameState.LOST)
         }
     }
 
-    const areRetriesEnabled = () => numberOfRetries < maxNumberOfRetries
+    const areRetriesEnabled = () => numberOfRetries < MAX_NUMBER_OF_RETRIES
 
     const areSelectedNumbersCorrect = () => {
         const sumOfSelectedNumbers = selectedNumbers.reduce((a, b) => a + b, 0)
@@ -81,10 +98,11 @@ const Game: React.FC = () => {
     const restartGame = () => {
         setRoundNumber(1)
         setAvailableNumbers(Array.from(Array(9), (_, i) => i + 1))
-        setNumberOfStars(randomSum(availableNumbers, maxNumberOfStars))
+        setNumberOfStars(randomSum(availableNumbers, MAX_NUMBER_OF_STARS))
         setSelectedNumbers([])
         setNumberOfRetries(0)
         setGameState(GameState.IN_PROGRESS)
+        setTimer(GAME_TIME)
     }
 
     return (
@@ -97,7 +115,7 @@ const Game: React.FC = () => {
                 </ButtonArea>
                 <ButtonArea>
                     <CircularButton icon={faReply} isBlocked={!areRetriesEnabled()} onClick={retryDraw}>
-                        <CornerCircularLabel value={maxNumberOfRetries - numberOfRetries} corner={Corner.TOP_RIGHT}/>
+                        <CornerCircularLabel value={MAX_NUMBER_OF_RETRIES - numberOfRetries} corner={Corner.TOP_RIGHT}/>
                     </CircularButton>
                 </ButtonArea>
                 < SelectedNumbersDisplay
@@ -110,9 +128,9 @@ const Game: React.FC = () => {
             </div>
             <div className="third-row">
                 <div>
-                    {gameState !== GameState.IN_PROGRESS ?
-                        gameState === GameState.WON ?
-                            <Trophy onClick={restartGame}/> : <Bomb onClick={restartGame}/> : ''}
+                    {gameState === GameState.IN_PROGRESS ?
+                        <Timer timer={timer}/> :
+                        gameState === GameState.WON ? <Trophy onClick={restartGame}/> : <Bomb onClick={restartGame}/>}
                 </div>
             </div>
         </div>
