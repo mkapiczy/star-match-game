28d15dbc9b6354f1bcbabdf969b51af41c795b1e
Michal Kapiczynski <michal.kapiczynski@grapeup.com>
2020-01-14 14:57:40 +0100

    Update App.test.tsx to use describe and it

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Match Stars Game/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App tests", () => {
+  it("should render app", () => {
+    const { getByText } = render(<App />);
+    const linkElement = getByText(/Match Stars Game/i);
+    expect(linkElement).toBeInTheDocument();
+  })
+})
